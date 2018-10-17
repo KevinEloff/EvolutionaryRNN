26d12e228450d6cd18a4cfa63b81ca2e59652b4b
KevinEloff <k.eloff@hotmail.co.za>
2018-10-17 20:29:04 +0200

    Comment cleanup

--- Main.java
+++ Main.java
@@ -8,27 +8,32 @@ import java.util.ArrayList;
  */
 public class Main {
     
-    //private static float food_x, food_y, player_x, player_y;
-    //private static int[] nShape = {4, 32, 32, 4};
-    public static int[] nShape = {6, 64, 64, 4};
-    public static final int GENERATIONS = 100000;
-    public static final int TURNS = 35;
-    public static final int SAMPLES = 200;
-    public static final float MUTATION = 0.03f; //Old mute 0.1
-    public static final int MAP_SIZE = 10;
-    //public static final String INPUT = "out8-64-64-4-LINE.txt"; //in2 for double L
-    public static final String INPUT = "rnn-6-64-64-4-b-hard.txt"; //complex-8-64-64-4.txt
-    public static final String MAPNAME = "map_hard";
-    public static final boolean TRAIN = true;
+    public static int[] nShape              = {6, 64, 64, 4};               //Shape of neural network per-layer
+    public static final int GENERATIONS     = 100000;                       //Amount of generations to simulate
+    public static final int TURNS           = 35;                           //Maximum turns per episode
+    public static final int SAMPLES         = 200;                          //Amount of agents to simulate per generation
+    public static final float MUTATION      = 0.03f;                        //Rate of mutation of the agents
+    public static final int MAP_SIZE        = 10;                           //Size of map
+    public static final String INPUT        = "rnn-6-64-64-4-b-hard.txt";   //Optional input for weight initialization, leave as "" for random initial values
+    public static final String MAPNAME      = "map_hard";                   //Name of .png map to be loaded
+    public static final boolean TRAIN       = true;                         //Train new generations or just display result
     
     public static boolean[][] map;
     
     public static void main(String[] args) {
         
         NeuralNetwork t = new NeuralNetwork(nShape);
+        System.out.println("Network shape:");
         System.out.println(t.toString());
+        System.out.println("CONTROLS\n
+                    Training mode:\n
+                    \tP - Display current best generation\n
+                    \tX - End training early and go to viewing mode
+                    \tN - Display Neural Network evolving as it trains\n\n
+                    Viewing mode:\n
+                    \tSPACE - Save learnt weights to text file 'out.txt'\n\n");
         
-        //StdDraw.setCanvasSize(800, 800);
+        //StdDraw.setCanvasSize(800, 800); //Optional increase canvas size
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 1);
         StdDraw.setYscale(0, 1);
@@ -42,24 +47,23 @@ public class Main {
         if (!INPUT.isEmpty()) for (int i = 0; i < agents.length/4; i++)
             agents[i] = new Agent2(nShape, "weights/" + INPUT);
         
+        if (TRAIN) System.out.println("[TRAINING]");
+
         for (int generation = 0; generation < GENERATIONS && TRAIN; generation++) {
             
-            //init agents
-            for (Agent2 agent: agents) {
+            //Initialize agents for the generation
+            for (Agent2 agent: agents)
                 agent.score = 0;
-            }
+            
             for (int j = 0; j < 4; j++) {
                 int[] map_stuff = new int[4]; //Food x,y; Player x,y;
                 for (int i = 0; i < 4; i++)
                     map_stuff[i] = (int)(Math.random()*MAP_SIZE);
-                    //map_stuff[i] = (int)(Math.random()*4 + 6*(int)(Math.random()*2));
                     
-                //make sure not invalid
+                //make sure not invalid placement of target
                 while (map[map_stuff[1]][map_stuff[0]]) map_stuff[(int)(Math.random()*2)] = (int)(Math.random()*MAP_SIZE);
                 while (map[map_stuff[3]][map_stuff[2]]) map_stuff[(int)(Math.random()*2)+2] = (int)(Math.random()*MAP_SIZE);
                 
-//                map_stuff[2] = 0;
-//                map_stuff[3] = 0;
                 for (Agent2 agent: agents) {
                     agent.x = map_stuff[2];
                     agent.y = map_stuff[3];
@@ -97,9 +101,8 @@ public class Main {
                 agents[i+SAMPLES/4*2].mutate(MUTATION*4);
             }
             
-            if ((float)(generation)/5 == generation/5) {
+            if ((float)(generation)/5 == generation/5)
                 System.out.println("[Generation " + generation + "]: score = " + agents[0].score);
-            }
             
             
             if (StdDraw.isKeyPressed(KeyEvent.VK_N)) {
@@ -112,14 +115,9 @@ public class Main {
                 int[] map_stuff = new int[4]; //Food x,y; Player x,y;
                 for (int i = 0; i < 4; i++)
                     map_stuff[i] = (int)(Math.random()*MAP_SIZE);
-                    //map_stuff[i] = (int)(Math.random()*4 + 6*(int)(Math.random()*2));
-                //*4 + 6
-//                map_stuff[2] = 0;
-//                map_stuff[3] = 0;
                 
                 while (map[map_stuff[1]][map_stuff[0]]) map_stuff[(int)(Math.random()*2)] = (int)(Math.random()*MAP_SIZE);
                 while (map[map_stuff[3]][map_stuff[2]]) map_stuff[(int)(Math.random()*2)+2] = (int)(Math.random()*MAP_SIZE);
-                 
                 
                 agents[0].x = map_stuff[2];
                 agents[0].y = map_stuff[3];
@@ -127,8 +125,6 @@ public class Main {
                 agents[0].lx = 0;
                 agents[0].ly = 0;
                 agents[0].reset();
-                agents[1].x = map_stuff[2];
-                agents[1].y = map_stuff[3];
                 
                 for (int i = 0; i < TURNS; i++) {
                     StdDraw.clear();
@@ -138,22 +134,15 @@ public class Main {
                     StdDraw.setPenColor(Color.BLACK);
                     agents[0].move(map_stuff[0], map_stuff[1]);
                     agents[0].draw();
-                    agents[1].move(map_stuff[0], map_stuff[1]);
-                    agents[1].draw();
 
                     StdDraw.setPenColor(Color.RED);
                     StdDraw.filledSquare((map_stuff[0]+0.5)/(double)MAP_SIZE, (map_stuff[1]+0.5)/(double)MAP_SIZE, 0.4/(double)MAP_SIZE);
-                    
-//                    StdDraw.setPenColor(Color.GREEN);
-//                    StdDraw.filledSquare((5)/(double)Main.MAP_SIZE, (5)/(double)Main.MAP_SIZE, 1/(double)Main.MAP_SIZE);
-                    
-                    //agents[0].drawNetwork(0.1f, 0.1f, 0.8f);
                     
                     if (StdDraw.isKeyPressed(KeyEvent.VK_SPACE))
                         agents[0].save("out.txt");
 
                     StdDraw.show();
-                    StdDraw.pause(50);
+                    StdDraw.pause(20);
                 }
             }
             
@@ -161,25 +150,17 @@ public class Main {
                 break;
         }
         
-        
+        //Display Mode
         while (true) {
             int[] map_stuff = new int[4]; //Food x,y; Player x,y;
             for (int i = 0; i < 4; i++)
                 map_stuff[i] = (int)(Math.random()*MAP_SIZE);
-                //map_stuff[i] = (int)(Math.random()*4 + 6*(int)(Math.random()*2));
             
             while (map[map_stuff[1]][map_stuff[0]]) map_stuff[(int)(Math.random()*2)] = (int)(Math.random()*MAP_SIZE);
             
-//            map_stuff[2] = 0;
-//            map_stuff[3] = 0;
-            
-            //agents[0].x = map_stuff[2];
-            //agents[0].y = map_stuff[3];
             agents[0].lx = 0;
             agents[0].ly = 0;
             agents[0].reset();
-            agents[1].x = map_stuff[2];
-            agents[1].y = map_stuff[3];
             
             for (int i = 0; i < TURNS; i++) {
                 StdDraw.clear();
@@ -189,14 +170,9 @@ public class Main {
                 StdDraw.setPenColor(Color.BLACK);
                 agents[0].move(map_stuff[0], map_stuff[1]);
                 agents[0].draw();
-                agents[1].move(map_stuff[0], map_stuff[1]);
-                //agents[1].draw();
                 
                 StdDraw.setPenColor(Color.RED);
                 StdDraw.filledSquare((map_stuff[0]+0.5)/(double)MAP_SIZE, (map_stuff[1]+0.5)/(double)MAP_SIZE, 0.4/(double)MAP_SIZE);
-                
-//                StdDraw.setPenColor(Color.GREEN);
-//                StdDraw.filledSquare((5)/(double)Main.MAP_SIZE, (5)/(double)Main.MAP_SIZE, 1/(double)Main.MAP_SIZE);
                 
                 if (StdDraw.isKeyPressed(KeyEvent.VK_R)) {
                     agents[0].x = (int)(Math.random()*4 + 6*(int)(Math.random()*2));
